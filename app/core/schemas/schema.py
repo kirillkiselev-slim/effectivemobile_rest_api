d41dafa873c25271088dd1edccd4e9a65a90f5ab
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2024-09-27 22:11:50 +0300

    add schemas to the project

---
+++ app/core/schemas/schema.py
@@ -0,0 +1,47 @@
+import datetime
+
+from pydantic import BaseModel, fields, field_validator
+
+from api.constants import ALLOWED_STATUSES
+
+
+class BaseConfigModel(BaseModel):
+
+    class Config:
+        orm_mode = True
+
+
+class ProductBase(BaseConfigModel):
+    name: str = fields.Field(max_length=255, min_length=3)
+    description: str | None = None
+    price: int = fields.Field(ge=1)
+    amount_left: int = fields.Field(ge=0)
+
+
+class ProductGet(ProductBase):
+    id: int = fields.Field()
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class OrderBase(BaseConfigModel):
+    created_at: datetime.datetime = datetime.datetime.now(datetime.UTC)
+    status: str = fields.Field(
+        min_length=5, examples=[ALLOWED_STATUSES])
+
+
+class OrderGet(OrderBase):
+    id: int
+
+
+class OrderCreate(OrderBase):
+
+    @field_validator('status')
+    @classmethod
+    def check_status(cls, status: str) -> str:
+        assert status.lower() in ALLOWED_STATUSES,\
+            f'{status} not in allowed statuses'
+        return status.lower()
+
