27d682a53079b725e69c0898e01209310f08a54b
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2024-09-30 19:59:38 +0300

    add main.py and modify models.py

--- app/main.py
+++ app/main.py
@@ -1,29 +1,30 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
 from fastapi import FastAPI
 
 from app.v1.api.endpoints import products, orders
 from app.v1.api import api
-from app.core.models.database import Base, engine, async_session
+from app.core.models.database import sessionmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Function that handles startup and shutdown events.
+    """
+    yield
+    if sessionmanager._engine is not None:
+        await sessionmanager.close()
+
+
+app = FastAPI(lifespan=lifespan, title='Склад')
 
 api_start = api
+
 app.include_router(products)
 app.include_router(orders)
 
 
-async def init_db():
-    """Инициализация БД, создание всех таблиц."""
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-@app.on_event('startup')
-async def on_startup():
-    """Запускает инициализацию БД."""
-    await init_db()
-
-
-# async def get_db():
-#     """Получение сессии БД для каждого запроса."""
-#     async with async_session() as session:
-#         yield session
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='127.0.0.1', reload=True, port=8000)
